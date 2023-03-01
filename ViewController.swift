5bf6acb3a93ca80d0d80b6875b13b6be85fe76d7
Nikita-Ananev <74005578+Nikita-Ananev@users.noreply.github.com>
2023-03-01 21:40:00 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Никита Ананьев on 27.02.2023.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
